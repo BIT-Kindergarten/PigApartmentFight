81a861d5d569c22bb59237b6be5bd92c7e3116d9
peterlong0612 <peterlong0612@outlook.com>
2020-09-20 13:58:56 +0800

    Update PathGenerate.cs

--- Scripts/PathGenerate.cs
+++ Scripts/PathGenerate.cs
@@ -9,7 +9,8 @@ public class PathGenerate : MonoBehaviour
     public GameObject ene2;
 
     //每走一步，一个cube，生成的环境素材和数量
-    int environ_num = 2; 
+    public bool plant = false;
+    int environ_num = 1;
     public GameObject environ1;
     public GameObject environ2;
     public GameObject environ3;
@@ -17,6 +18,11 @@ public class PathGenerate : MonoBehaviour
     //public GameObject environ1;
     //public GameObject environ1;
     //public GameObject environ1;
+
+    public GameObject[] trees ;
+
+    bool[,] pots = new bool[200,200];
+    int lenpots = 0;
 
     int j, temp, mask = 0;   // mask是否需要转向
     int x, y;
@@ -27,37 +33,56 @@ public class PathGenerate : MonoBehaviour
     int x1, y1;   // 怪物位置，在此声明以便//GenerateTree中可使用
     int x2, y2;
 
-    private int abs(int a )
+    private int abs(int a)
     {
         return a > 0 ? a : -a;
+    }
+
+    private void initpots()
+    {
+        trees = new GameObject[] {environ1, environ2, environ3 };
+
+        for(int i= 0; i < 200; i++)
+        {
+            for(int j = 0; j < 200; j++)
+            {
+                pots[i, j] = true;
+            }
+        }
+    }
+
+    private void addpots(int x, int y)
+    {
+        pots[x, y] = false;
+        lenpots += 1;
     }
 
     // 判断(a,b)与(x,y)距离是否足够 d 远
     private bool isFar(float a, float b, float x, float y, float d)
     {
-        return ( (a - x) *(a-x) + (b - y)*(b-y) ) >= d * d ? true : false;
+        return ((a - x) * (a - x) + (b - y) * (b - y)) >= d * d ? true : false;
     }
 
     // 道路两边生成树、石头等场景元素
     private void GenerateTree(int x, int y)
     {
+        int limit = 1000;
         for (int i = 0; i < environ_num; i++)
         {
             int tx = Random.Range(x - 20, x + 20);
             int ty = Random.Range(y - 20, y + 20);
-            if (isFar(tx, ty, x1, y1, 10) && isFar(tx, ty, x, y, 4 ) )
+            if (isFar(tx, ty, x1, y1, 10) && isFar(tx, ty, x, y, 4) )
             {
-                Instantiate(environ1, new Vector3(tx, 0, ty), Quaternion.identity);
-                Instantiate(environ2, new Vector3(tx, 0, ty), Quaternion.identity);
-                Instantiate(environ3, new Vector3(tx, 0, ty), Quaternion.identity);
+                Instantiate(trees[Random.Range(0,3)], new Vector3(tx, 0, ty), Quaternion.identity);
             }
         }
     }
 
-    private void GeneratePath(int enx, int eny)
+    private void GeneratePath1(int enx, int eny)
     {
         //while( isFar(x,y,enx,eny,10) )
-        while ((xreal <= eny) || (yreal >= enx))  // x、y依次到达一次停止
+        //while ((xreal <= eny) || (yreal >= enx))  // x、y依次到达一次停止
+        while ((yreal <= eny) || (xreal >= enx))  // x、y依次到达一次停止
         {
 
             if (temp == 1)
@@ -67,30 +92,35 @@ public class PathGenerate : MonoBehaviour
                 {
                     for (int i = 0; i < j; i++)
                     {
-                        Instantiate(Pobj, new Vector3(y, 0, x + i), Quaternion.identity);
-                        //GenerateTree(x, y); // 每走一步生成tree
+                        int a = x, b = y + i;
+                        Instantiate(Pobj, new Vector3(a, 0, b), Quaternion.identity);
+                        addpots(a, b);
+                        if(plant) GenerateTree(a, b); // 每走一步生成tree
                     }
-                    temp = 2;
-                    x = x + j;
-                    if (xreal <= eny)
-                        xreal = x;
-                    if (y < enx)
+                    //temp = 2;
+                    y = y + j;
+                    if (yreal <= eny)
+                        yreal = y;
+                    if (x < enx)
                         mask = 1;
                     else
                         mask = 0;
                 }
-                else
+                else  // mask=1
                 {
                     for (int i = 0; i < j; i++)
                     {
-                        Instantiate(Pobj, new Vector3(y, 0, x - i), Quaternion.identity);
-                        //GenerateTree(x, y);
+                        int a = x, b = y - i;
+                        Instantiate(Pobj, new Vector3(a, 0, b), Quaternion.identity);
+                        addpots(a, b);
+                        if(plant) GenerateTree(a, b);
                     }
-                    temp = 2;
-                    x = x - j;
+                    //temp = 2;
+                    y = y - j;
 
                     mask = 0;
                 }
+                temp = 2;
             }
             else if (temp == 2)
             {
@@ -99,27 +129,32 @@ public class PathGenerate : MonoBehaviour
                 {
                     for (int i = 0; i < j; i++)
                     {
-                        Instantiate(Pobj, new Vector3(y - i, 0, x), Quaternion.identity);
-                        //GenerateTree(x, y);
+                        int a = x - i, b = y;
+                        Instantiate(Pobj, new Vector3( a, 0, b), Quaternion.identity);
+                        addpots(a, b);
+                        
+                        if(plant) GenerateTree(a, b);
                     }
                     temp = 1;
-                    y = y - j;
-                    if (yreal >= enx)
-                        yreal = y;
-                    if (x <= eny)
+                    x = x - j;
+                    if (xreal >= enx)
+                        xreal = x;
+                    if (y <= eny)
                         mask = 0;
                     else
                         mask = 1;
                 }
-                else
+                else // temp  =2 , mask = 1;
                 {
                     for (int i = 0; i < j; i++)
                     {
-                        Instantiate(Pobj, new Vector3(y + i, 0, x), Quaternion.identity);
-                        //GenerateTree(x, y);
+                        int a = x + i, b = y;
+                        Instantiate(Pobj, new Vector3( a, 0, b), Quaternion.identity);
+                        addpots(a, b);
+                        if(plant) GenerateTree(a, b);
                     }
                     temp = 1;
-                    y = y + j;
+                    x = x + j;
                     mask = 0;
                 }
 
@@ -129,34 +164,137 @@ public class PathGenerate : MonoBehaviour
         }
     }
 
+    //private void GeneratePath2()
+    //{
+    //    while ((xreal <= y2) || (yreal <= x2))
+    //    {
+
+    //        if (temp == 1)
+    //        {
+    //            j = Random.Range(5, 10);
+    //            if (mask == 0)
+    //            {
+    //                for (int i = 0; i < j; i++)
+
+    //                {
+
+    //                    Instantiate(obj, new Vector3(y, 0, x + i), Quaternion.identity);
+
+
+    //                }
+    //                temp = 2;
+    //                x = x + j;
+    //                x = x + 1;
+    //                if (xreal <= y2)
+    //                    xreal = x;
+    //                if (y > x2)
+    //                {
+    //                    mask = 1;
+    //                    y = y + 1;
+    //                }
+
+    //                else
+    //                {
+    //                    mask = 0;
+    //                    y = y - 1;
+    //                }
+    //            }
+    //            else
+    //            {
+    //                for (int i = 0; i < j; i++)
+
+    //                {
+
+    //                    Instantiate(obj, new Vector3(y, 0, x - i), Quaternion.identity);
+
+
+    //                }
+    //                temp = 2;
+    //                x = x - j;
+    //                y = y - 1;
+    //                x = x - 1;
+
+    //                mask = 0;
+
+    //            }
+
+
+    //        }
+    //        else if (temp == 2)
+    //        {
+
+    //            j = Random.Range(5, 10);
+    //            if (mask == 0)
+    //            {
+    //                for (int i = 0; i < j; i++)
+    //                {
+    //                    Instantiate(obj1, new Vector3(y + i, 0, x), Quaternion.identity);
+    //                }
+    //                temp = 1;
+    //                y = y + j;
+    //                y = y + 1;
+
+    //                if (yreal <= x2)
+    //                    yreal = y;
+    //                if (x <= y2)
+    //                {
+    //                    mask = 0;
+    //                    x = x - 1;
+    //                }
+    //                else
+    //                {
+    //                    mask = 1;
+    //                    x = x + 1;
+    //                }
+    //            }
+    //            else
+    //            {
+    //                for (int i = 0; i < j; i++)
+    //                {
+    //                    Instantiate(obj1, new Vector3(y - i, 0, x), Quaternion.identity);
+    //                }
+    //                temp = 1;
+    //                y = y - j;
+    //                y = y - 1;
+    //                x = x - 1;
+    //                mask = 0;
+    //            }
+
+    //        }
+    //        else
+    //            break;
+    //    }
+    //}
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initpots(); // 初始化数组，每个点可放道具
+
         x = 40;// 初始点位置
         y = 40;// y实际是z
-        
+
         temp = Random.Range(1, 3);  //方向
 
-        x1 = Random.Range(40, 150);//  怪物位置
-        y1 = Random.Range(100, 175);
+        x1 = Random.Range(40, 100);//  怪物位置
+        y1 = Random.Range(100, 160);
         Instantiate(ene1, new Vector3(x1, 0, y1), Quaternion.identity); // 生成怪物
-        x2 = Random.Range(160, 180);//  怪物位置
-        y2 = Random.Range(150, 160);
+        x2 = Random.Range(100, 160);//  怪物位置
+        y2 = Random.Range(40, 100);
         Instantiate(ene2, new Vector3(x2, 0, y2), Quaternion.identity); // 生成怪物
 
         //x2 = Random.Range(-50, -29);
         //y2 = Random.Range(30, 40);
 
-        GeneratePath(x1,y1);
+        GeneratePath1(x1, y1);
 
-        int Cx = 100;
-        int Cy = 100;
-        // if (Cx!=x)
-        x += abs(Cx-x1)/(Cx-x) *(50-40) ;
-        y += abs(Cy - y1) / (Cy - y)  * (175 - 160);
+        //int Cx = 100;
+        //int Cy = 100;
+        //// if (Cx!=x)
+        //x += abs(Cx - x1) / (Cx - x) * (50 - 40);
+        //y += abs(Cy - y1) / (Cy - y) * (175 - 160);
 
-        GeneratePath(x2, y2);
+        //GeneratePath(x2, y2);
 
     }
 
